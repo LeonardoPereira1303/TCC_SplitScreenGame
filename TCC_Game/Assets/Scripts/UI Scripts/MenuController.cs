8a3de0a15660ab83223d8179c0618a48cedac056
Leonardo <leo_santos1304@hotmail.com>
2025-01-22 12:31:52 -0300

    MainMenu: Configuração dos Botões
    Todos os botões estão funcionando perfeitamente com seus respectivos paineis

--- TCC_Game/Assets/Scripts/UI Scripts/MenuController.cs
+++ TCC_Game/Assets/Scripts/UI Scripts/MenuController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Levels To Load")]
+    public string newGameLevel;
+    private string levelToLoad;
+
+    [SerializeField] private GameObject noSavedGame = null;
+
+    //The Button YES will load the scene "Gameplay"
+    public void NewGameDialogueYes()
     {
-        
+        SceneManager.LoadScene(newGameLevel);
     }
 
-    // Update is called once per frame
-    void Update()
+    //The Button YES will load the scene "Gameplay" where was save
+    public void LoadGameDialogueYes()
     {
-        
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+
+            //To load a saved scene, need one more line for it and the script for Save/Load Game
+        }
+        else
+        {
+            noSavedGame.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 }
