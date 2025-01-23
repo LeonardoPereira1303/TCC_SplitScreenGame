ef6e64ba12f21ea1465ef18a26c5082f0244f6a3
Leonardo <leo_santos1304@hotmail.com>
2025-01-23 13:53:18 -0300

    MainMenu: Settings Config
    Continuando a configuração do painel de settings aplicando um script e dois novos botões

--- TCC_Game/Assets/Scripts/UI Scripts/MenuController.cs
+++ TCC_Game/Assets/Scripts/UI Scripts/MenuController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Settings")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels To Load")]
     public string newGameLevel;
     private string levelToLoad;
-
     [SerializeField] private GameObject noSavedGame = null;
 
     //The Button YES will load the scene "Gameplay"
@@ -37,5 +44,24 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
