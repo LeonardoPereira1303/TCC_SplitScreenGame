e724f80234ae9cc083880b91bab61a0dee4cefd0
Leonardo <leo_santos1304@hotmail.com>
2025-01-23 15:54:08 -0300

    MainMenu: Finalizado e Build
    Finalizei a configuração base do MainMenu. Todos os botões funcionando perfeitamente e testados, também, em build.

--- TCC_Game/Assets/Scripts/UI Scripts/MenuController.cs
+++ TCC_Game/Assets/Scripts/UI Scripts/MenuController.cs
@@ -5,19 +5,62 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Rendering;
 using TMPro;
+using UnityEngine.Experimental.Rendering;
 
 public class MenuController : MonoBehaviour
 {
     [Header("Volume Settings")]
     [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
 
     [SerializeField] private GameObject confirmationPrompt = null;
+
+    [Header("Graphics Settings")]
+    private int qualityLevel;
+    private bool _isFullScreen;
 
     [Header("Levels To Load")]
     public string newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGame = null;
+
+    [Header("Resolution Dropdown")]
+    public TMP_Dropdown resolutionDropdown;
+    public Resolution[] resolutions;
+
+    [Space(10)]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option  = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     //The Button YES will load the scene "Gameplay"
     public void NewGameDialogueYes()
@@ -52,6 +95,33 @@ public class MenuController : MonoBehaviour
         volumeTextValue.text = volume.ToString("0.0");
     }
 
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            //Reset audio values
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+        if(MenuType == "Graphics")
+        {
+            //Reset quality values
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GraphicsAplly();
+        }
+    }
+
     public void VolumeApply()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
@@ -63,5 +133,28 @@ public class MenuController : MonoBehaviour
         confirmationPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
         confirmationPrompt.SetActive(false);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        _isFullScreen = isFullScreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        qualityLevel = qualityIndex;
+    }
+
+    public void GraphicsAplly()
+    {
+        //Apply the quality in game
+        PlayerPrefs.SetInt("masterQuality" , qualityLevel);
+        QualitySettings.SetQualityLevel(qualityLevel);
+
+        //Apply the fullscreen in game
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0));
+        Screen.fullScreen = _isFullScreen;
+
+        StartCoroutine(ConfirmationBox());
     }
 }
