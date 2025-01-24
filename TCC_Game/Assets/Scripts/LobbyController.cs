25c5592f206c069b6801fab1b3e2b1fdcf8cf86e
Leonardo <leo_santos1304@hotmail.com>
2025-01-24 10:00:10 -0300

    Lobby: Configuração
    Apliquei um script de Lobby no LobbyManager e adicionei o prefab do player ao LobbyManager para adicionar os jogadores na cena

---
+++ TCC_Game/Assets/Scripts/LobbyController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class LobbyController : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public TMP_Text lobbyStatusText;
+    public Button startGameButton;
+
+    [Header("Lobby Settings")]
+    public int maxPlayers = 4;
+
+    private List<PlayerInput> players = new List<PlayerInput>();
+
+    void Start()
+    {
+        // Initialize UI
+        UpdateLobbyStatus();
+
+        // Disable start button initially
+        startGameButton.interactable = false;
+        startGameButton.onClick.AddListener(StartGame);
+
+        // Subscribe to player joining events
+        PlayerInputManager.instance.onPlayerJoined += OnPlayerJoined;
+        PlayerInputManager.instance.onPlayerLeft += OnPlayerLeft;
+    }
+
+    void OnPlayerJoined(PlayerInput playerInput)
+    {
+        if (players.Count < maxPlayers)
+        {
+            players.Add(playerInput);
+            Debug.Log(playerInput.playerIndex + " joined the lobby.");
+            UpdateLobbyStatus();
+        }
+        else
+        {
+            Debug.Log("Lobby is full. Player " + playerInput.playerIndex + " cannot join.");
+            Destroy(playerInput.gameObject);
+        }
+    }
+
+    void OnPlayerLeft(PlayerInput playerInput)
+    {
+        players.Remove(playerInput);
+        Debug.Log(playerInput.playerIndex + " left the lobby.");
+        UpdateLobbyStatus();
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Game is starting with players:");
+        foreach (var player in players)
+        {
+            Debug.Log("Player " + player.playerIndex);
+        }
+
+        // Here, load your game scene or initialize game logic.
+        Debug.Log("Game started!");
+    }
+
+    void UpdateLobbyStatus()
+    {
+        lobbyStatusText.text = "Players in Lobby: " + players.Count + "/" + maxPlayers;
+
+        // Enable or disable start button based on player count
+        startGameButton.interactable = players.Count > 1;
+    }
+
+    void OnDestroy()
+    {
+        if (PlayerInputManager.instance != null)
+        {
+            PlayerInputManager.instance.onPlayerJoined -= OnPlayerJoined;
+            PlayerInputManager.instance.onPlayerLeft -= OnPlayerLeft;
+        }
+    }
+}
