12680abb18e4754c744388752e76e5f425a04c95
Leonardo <leo_santos1304@hotmail.com>
2025-02-27 10:54:15 -0300

    Menu - Background
    Tentativa de fazer um menu dinâmico. Além disso, os botões pararam de funcionar

--- TCC_Game/Assets/Scripts/UI Scripts/BackgroundDynamic.cs
+++ TCC_Game/Assets/Scripts/UI Scripts/BackgroundDynamic.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BackgroundDymaic : MonoBehaviour
+public class BackgroundDynamic : MonoBehaviour
 {
+    public CanvasGroup secondaryScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideScreen();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowScreen()
     {
-        
+        secondaryScreen.alpha = 1;
+        secondaryScreen.interactable =  true;
+        secondaryScreen.blocksRaycasts = true;
+    }
+
+    public void HideScreen()
+    {
+        secondaryScreen.alpha = 0;
+        secondaryScreen.interactable =  false;
+        secondaryScreen.blocksRaycasts = false;
     }
 }
